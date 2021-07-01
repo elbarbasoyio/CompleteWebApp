bcbae288e51bf28c168b5294c8e296657b74b16b
Leonardo83 <pogomaligno@gmail.com>
2021-06-30 22:10:43 -0300

    Javascript + Jquery are done

---
+++ DutchTreat/wwwroot/js/Index.js
@@ -0,0 +1,33 @@
+﻿$(document).ready(function () {
+    var $theForm = $("#theForm");
+    $theForm.hide();
+
+    var $buyButton = $("#buyButton");
+    $buyButton.on("click", function () {
+        alert("Buying Item!");
+    });
+
+    var $productInfo = $(".product-props li");
+    $productInfo.on("click", function () {
+        console.log("You clicked on " + $(this).text());
+    });
+
+
+    var $loginToggle = $("#loginToggle");
+    var $popupForm = $(".popup-form");
+
+    $loginToggle.on("click", function () {
+        $popupForm.fadeToggle(1000);
+    })
+
+
+
+
+
+
+
+
+
+
+
+});

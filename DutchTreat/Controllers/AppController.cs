ee89d9b21cc80ccabe709ceb47f95e723fb1083c
Leonardo83 <pogomaligno@gmail.com>
2021-07-02 20:01:11 -0300

    Module 5 completed ::

---
+++ DutchTreat/Controllers/AppController.cs
@@ -0,0 +1,50 @@
+﻿using DutchTreat.Models;
+using DutchTreat.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DutchTreat.Controllers
+{
+    public class AppController : Controller
+    {
+        private readonly IMailService _mailService;
+
+        public AppController(IMailService mailService)
+        {
+            _mailService = mailService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet("contact")]
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost("contact")]
+        public IActionResult Contact(ContactViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                //send email
+                _mailService.SendMessage("arivas.everis@gmail.com", model.Subject, $"From: {model.Name} - {model.Email}, Message: {model.Message}");
+                ViewBag.UserMessage = "Mail Sent!";
+                ModelState.Clear();
+            }
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewBag.Title = "About";
+            return View();
+        }
+
+    }
+}
--- DutchTreat/Startup.cs
+++ DutchTreat/Startup.cs
@@ -1,3 +1,4 @@
+using DutchTreat.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -22,14 +23,32 @@ namespace DutchTreat
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            
+            services.AddTransient<IMailService, NullMailService>();
+            services.AddControllersWithViews()
+                .AddRazorRuntimeCompilation();
+            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UseDefaultFiles();
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/error");
+            }
             app.UseStaticFiles();
+            app.UseRouting();
+            app.UseEndpoints(cfg => 
+            {
+                cfg.MapRazorPages();
+                cfg.MapControllerRoute("Default",
+                    "/{controller}/{action}/{id?}",
+                    new { controller = "App", action = "Index"});   
+            });
 
         }
     }
